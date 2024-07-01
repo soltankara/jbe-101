3f64cef58a99c830c86c833cdbcc4f729df36ea5
alizadamuhammed <alizadamhmmd25@gmail.com>
2024-07-02 00:12:47 +0400

    Homework 2

---
+++ Muhammed.java
@@ -0,0 +1,56 @@
+public class Muhammed {
+    public static void main(String[] args) {
+        //Car
+        String model = "Toyota Supra";
+        byte numberOfDoors = 2;
+        String color = "Black";
+        long price;
+        short dateOfProduction = 2017;
+        boolean crash = false;
+        int distanceKM = 232545;
+        //Book
+        short pageCount; // Longest book in the world is One Piece (21450 pages)
+        String author = "George Orwell";
+        int priceDollar = 12;
+        String type;
+        short publicationYear = 1939;
+        String title;
+        //Student
+        String name = "Muhammed";
+        byte classYear = 11;
+        long idOfStudent;
+        short birthDate;
+        //Table
+        byte numberOfLegs = 3;
+        String material;
+        int lengthMM;
+        short productionYear = 2012;
+        String color2 = "White";
+        //TV
+        String brand = "LG";
+        String modelName;
+        long serialID;
+        short screenSizeWithInches;
+        boolean smart = true;
+        short ProductionYear2 = 2021;
+        // Phone
+        String brand2 = "Apple";
+        String model2 = "Iphone 13";
+        byte Cameras = 2;
+        long imei;
+        byte batteryCapacityPercent = 86;
+        short productionYear3 = 2021;
+        // Bicycle
+        String model3;
+        short lengthCM;
+        byte wheelSize;
+        String color3 = "Black";
+        short maximumCapacityKG = 250;
+        // Scooter
+        String model4;
+        String color4 = "Red";
+        short maximumCapacityKG2 = 250;
+        byte maximumDistanceKM = 50;
+        short maximumSpeedKMH = 60;
+    }
+}
