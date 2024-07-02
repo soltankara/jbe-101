176c6f767299481f3ee160faaa4e036626bbaf8a
Ali <haqverdiyevlii097@gmail.com>
2024-07-02 18:54:15 +0400

    Task 2

---
+++ Main.java
@@ -0,0 +1,75 @@
+public class Main {
+    public static void main(String[] args) {
+        //sualim olacaq-string xeta verir mende
+        //car
+        int numberohwheel=4;
+        double surfaceArea=7000.25;
+        boolean hasHatch=true;
+        byte numberofRule=1;
+        long price=350000l;
+        float luggageArea=1001.5f;
+        String name="car";
+        //book
+        int numberofPage=200;
+        double widthoftheBook= 10.5;
+        boolean hasPage=true;
+        byte numberofFaces=2;
+        long price2=9000l;
+        float thelengthofBook=40.6f;
+        String name5="book";
+
+
+        //student
+        double heightLength=185.5;
+        boolean hasbrain=true;
+        byte numberofnoise=1;
+        long age=18l;
+        float faceLength=22.5f;
+        int numberofbrain=1;
+        String  name6="student";
+        //door
+        int tutacaq=1;
+        double uzunluq=200.5;
+        boolean tutacaqvar=true;
+        byte kilidsayi=2;
+        long en=80l;
+        float diognal=183.5f;
+        String NAME="door";
+
+
+        //home
+        int  qapisayi=5;
+        byte penceresayi=6;
+        double evuzunluq=1800.25;
+        boolean damyoxdu=false;
+        long eveni=800;
+        float hundurluk=200.5f;
+        String name101="home";
+
+
+        //table
+        int ayaq=4;
+        byte uzluk=1;
+        double uzumluq=240.8;
+        boolean ayaqvar=true;
+        long eni= 100l;
+        float H=120.5f;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
