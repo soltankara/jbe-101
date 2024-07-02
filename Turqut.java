10bf3cf6e54bcf6a6f7d26f17e555979a086d972
Turqut7 <turqutismayil007@gmail.com>
2024-07-02 16:57:21 +0400

    Homework1
    This is homework1

---
+++ Turqut.java
@@ -0,0 +1,67 @@
+public class Turqut {
+    public static void main(String[] args) {
+        //Car
+        String brand = "KIA";
+        int numberOfSeats = 6;
+        long price = 1000000L;
+        double mileAge = 245.721D;
+        boolean hasSmartLock = false;
+
+        //Book
+        String author = "Mark Twen";
+        int pagesCount = 465;
+        double priceBook = 12.29D;
+        long numberOfCirculations = 250000L;
+
+        //Student
+        String name = "John Doe";
+        char grade = 'B';
+        int age = 20;
+        float entryPoint = 654.1F;
+        boolean hasGirl = false;
+
+        //Table
+        int numberOfLegs = 4;
+        double height = 1.8D;
+        float width = 1.8F;
+        String shape = "square";
+        boolean hasTableCloth = false;
+
+        //TV
+        String model = "Samsung";
+        byte screenSize = 55;
+        int yearOfRelease = 2018;
+        double diagonalSize = 65.0D;
+        boolean hasSmartTV = true;
+
+        //Computer
+        String manufacturer = "Intel";
+        String processor = "Intel Core i7";
+        int memorySize = 16;
+        double hardDiskSize = 1024.0D;
+        boolean hasBluetooth = true;
+
+        //Phone
+        String brandPhone = "Samsung";
+        char modelSeries = 'S';
+        byte numberOfSimCards = 2;
+        double screenSizePhone = 6.0D;
+        int batteryCapacityInMAmp = 3000;
+        boolean hasNFC = true;
+
+        //Cat
+        String breed = "Persian";
+        int ageCat = 3;
+        double weightCat = 5.0D;
+        String colorCat = "White";
+        boolean hasTail = true;
+
+        //Dog
+        String breedDog = "Labrador Retriever";
+        int ageDog = 5;
+        double weightDog = 12.0D;
+        String colorDog = "Brown";
+        boolean hasEars = true;
+
+    }
+}
