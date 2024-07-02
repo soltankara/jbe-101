a2531a9c16c9e22e18abec862ae7c9a11b696ce2
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2024-07-02 15:23:11 +0400

    Company and Tree added(filled)

--- semmed/Company.java
+++ semmed/Company.java
@@ -1,4 +1,9 @@
 package semmed;
 
 public class Company {
+    String companyName = "InterStellar";
+    int membersInTotal = 7804;
+    long companyAreaInMeterSquare = 34000l;
+    boolean isHiring = false;
+    float ratingOutOfFive = 4.55f;
 }
--- semmed/Tree.java
+++ semmed/Tree.java
@@ -1,4 +1,8 @@
 package semmed;
 
 public class Tree {
+    boolean isConsideredOld = true;
+    int branches = 65;
+    String treeName = "Akasiya";
+    long leafes = 29994930l;
 }
