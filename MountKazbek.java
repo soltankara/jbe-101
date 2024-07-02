d83cfaa7b0152dcab6a33c1d325ccec321165cb0
Anar Kheyrullayev <kheyrullayev.anar@gmail.com>
2024-07-02 16:36:11 +0400

    MountKazbek.java

---
+++ MountKazbek.java
@@ -0,0 +1,11 @@
+public class MountKazbek {
+    public static void main(String[] args) {
+        // Mount Kazbek
+        String Name = "Kazbek";
+        String Species = "Mount";
+        int ElevationMetr = 5054;
+        long ProminenceMetr = 2.353l;
+        float IsolationMetr = 113.6f;
+
+    }
+}
