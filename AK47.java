9670712dd84e451d933429c2b49178e311956ccf
Anar Kheyrullayev <kheyrullayev.anar@gmail.com>
2024-07-02 16:37:40 +0400

    AK47.java

---
+++ AK47.java
@@ -0,0 +1,13 @@
+public class Ak47 {
+    public static void main(String[] args) {
+        //AK-47
+        String name = "AK-47";
+        String Type = "Assault rifle";
+        String Place = "Soviet Union";
+        double MassKilo = 3.47;
+        char TheFirstCharacter ='A';
+        int TheNumericPartOf = 47;
+        long TheYearAdopted =  1947;
+        float TheCaliberOfRifle = 7.62f;
+    }
+}
