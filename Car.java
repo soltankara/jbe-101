84b427d34f22b3b1f1e035a713c9900b7ffe7895
Adil İsmayılov <ismayilovadil670@gmail.com>
2024-07-02 13:38:55 +0400

    first commit

---
+++ Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    String brand = "BMW";
+    String model = "520";
+    String color = "black";
+    float weightInTons = 1.2f;
+    boolean autoEngine = true;
+    float engineVolumeInLitres = 2.0f;
+}
