4c4f0afb67c7fc7f9fd7737faab477c423cfbc6c
OmarTayyar <yunuszade005@gmail.com>
2024-07-02 18:07:50 +0400

    task 2

---
+++ Omar.java
@@ -0,0 +1,75 @@
+public class Omar {
+    public static void main(String[] args) {
+        //Car
+        String name = "BMW";
+        int wheels = 4;
+        float engine = 4.0f;
+        double fuel_consumption_per100 = 30.5;
+        byte number_of_seats = 4;
+        long price = 50000L;
+        boolean sport = true;
+        //Book
+        String ad = "Kosmos";
+        String author = "Karl Sagan";
+        int pages = 457;
+        double qiymet = 27.5;
+        float weight = 1.2f;
+        long histority = 15000L;
+        boolean science = true;
+        byte redactors = 2;
+        //Student
+        String Name = "Omar";
+        int age = 19;
+        boolean male = true;
+        double ceki = 65.7;
+        float height = 1.86f;
+        long birth_year = 2004;
+        byte number_of_brain = 1;
+        //Table
+        String color  = "Brown";
+        int legs = 4;
+        double surface_area = 56.7;
+        float kutle = 7.5f;
+        long price_1 = 25000L;
+        boolean made_by_hand = true;
+        byte number_of_corners = 4;
+        //Tv
+        String tv_name = "Bang Olufsen";
+        double screen_areaCM = 455.7;
+        int screen_height = 455;
+        long tv_price = 35000L;
+        byte functions = 2;
+        float heightM = 1.8f;
+        boolean smart_tv = true;
+        //Piano
+        String Model = "Steinway and sons";
+        int produce_year = 2000;
+        long price_p = 110000L;
+        double tiles = 83.5;
+        byte chair = 1;
+        boolean grand = true;
+        float widthM = 2.1f;
+        //Ronaldo
+        String name_C = "Cristiano";
+        float height_C = 1.8f;
+        long salary = 243000000L;
+        int goals = 1000;
+        boolean goat = true;
+        double total_wealthM = 100.34;
+        byte number = 7;
+        //Chopin
+        String type = "Composer";
+        int age_C = 37;
+        boolean virtuoso = true;
+        long pianist_level = 100000L;
+        float height_Ch = 1.7f;
+        double compositions = 70.0;
+        byte hands = 2;
+
+
+
+
+
+
+    }
+}
