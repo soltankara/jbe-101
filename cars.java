8d624c7133ba7cf93ecd763aea255c8d8f5867e0
Elshan Alasgarov <aleskerof00@gmail.com>
2024-07-02 15:38:11 +0400

    cars.java elave edildi.

---
+++ cars.java
@@ -0,0 +1,10 @@
+public class cars {
+    public static void main(String[] args) {
+        String carName = "Mercedes Benz";
+        int carSpeed = 300;
+        String carColor = "Metalic";
+        long carKm = 70000;
+        boolean carDamage = false;
+
+    }
+}
