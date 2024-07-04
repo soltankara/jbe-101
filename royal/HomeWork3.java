ddfede29ab8f3707019220d2faa5e03fae0a3bb6
RoyalMsyv <azemusayev97@gmail.com>
2024-07-04 18:26:15 +0400

    HomeWork3.java

---
+++ royal/HomeWork3.java
@@ -0,0 +1,23 @@
+   int eight=8;
+        int four=4;
+        System.out.println(8+4);
+
+        int fifteen=15;
+        int ten=10;
+        System.out.println(15-10);
+
+        int five=5;
+        int two=2;
+        System.out.println(5*2);
+
+        int eighteen=18;
+        int nine=9;
+        System.out.println(18/9);
+
+        // >, <, >=, <=, ==, !=
+        System.out.println(5==5);//true
+        System.out.println(9>10);//false
+        System.out.println(7<8);//true
+        System.out.println(5!=5);//false
+        System.out.println(5>=5);//true
+        System.out.println(7>=8);//false
