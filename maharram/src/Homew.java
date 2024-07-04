2deecd5d5568165fe1ebd55bc1d93080844a972c
Maharram <meherremheyderov04@gmail.com>
2024-07-05 01:45:17 +0400

    homework3 added this time

--- maharram/src/Homew.java
+++ maharram/src/Homew.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Homew {
+
+//Scanner 2
+
+import java.util.Scanner;
+
+public class Homew {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("enter your text :");
+
+        String text = scanner.nextLine();
+
+        System.out.print("enter symbol which you want to see index : ");
+
+        String symbol = scanner.nextLine();
+
+        System.out.println(" your symbol index is :" + text.indexOf(symbol));
+
+    }
+
+
 }
--- maharram/src/Main1.java
+++ maharram/src/Main1.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Main1 {
+
+//Scanner homework 1
+
+import java.util.Scanner;
+
+public class Main1 {
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.print("enter yout text ");
+        String text = scanner.nextLine();
+
+
+        System.out.print("enter index which you want change: ");
+        int index = scanner.nextInt();
+        scanner.nextLine();
+
+
+        System.out.print(" enter what you want to change ");
+        String newChar = scanner.nextLine();
+
+
+        String newtext = text.substring(0, index) + newChar + text.substring(index + 1);
+
+
+        System.out.println("newtext " + newtext);
+
+
+        scanner.close();
+
+
+    }
 }
+
