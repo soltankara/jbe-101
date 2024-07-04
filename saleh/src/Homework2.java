ec083556f9f3c56df317d47d2623edd7f07971c0
Saleh Samadli <realmadrid.rm171@gmail.com>
2024-07-04 16:49:06 +0400

    Homework2 ready Homework3 ready Homework4 ready

---
+++ saleh/src/Homework2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a text: ");
+        String text = sc.nextLine();
+        System.out.print("Enter char you want to see index: ");
+        char inputChar = sc.next().charAt(0);
+        int index = findCharIndex(text, inputChar);
+        if (index != -1) {
+            System.out.println("Index of '" + inputChar + "' in the text '" + text + "' is: " + index);
+        } else {
+            System.out.println("'" + inputChar + "' not found in the text '" + text + "'");
+        }
+        sc.close();
+    }
+
+    public static int findCharIndex(String text, char c) {
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == c) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ saleh/src/Homework3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first integer: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        int num2 = sc.nextInt();
+
+        if (num1 == num2)
+            System.out.println(num1 + " is equal to " + num2);
+        else
+            System.out.println(num1 + " is not equal to " + num2);
+
+        if (num1 > num2)
+            System.out.println(num1 + " is greater than " + num2);
+        else if (num1 < num2)
+            System.out.println(num1 + " is less than " + num2);
+
+        if (num1 >= num2)
+            System.out.println(num1 + " is greater than or equal to " + num2);
+
+        if (num1 <= num2)
+            System.out.println(num1 + " is less than or equal to " + num2);
+
+        sc.close();
+    }
+}
