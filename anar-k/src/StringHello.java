79ee9a14693c6a1574fc6408c0c03a2b554524e8
Anar Kheyrullayev <kheyrullayev.anar@gmail.com>
2024-07-04 17:10:01 +0400

    StringHello.java

---
+++ anar-k/src/StringHello.java
@@ -0,0 +1,17 @@
+String st1 = "Hello";
+        String st2 = "Hello";
+        String st3 = new String("Hello");
+        System.out.println(st1 == st2);
+        System.out.println(st1 == st3);
+true
+false
+why?
+
+String st1 = "Hello";
+        String st2 = "Hello";
+        String st3 = "Hello";
+        System.out.println(st1 == st2);
+        System.out.println(st1 == st3);
+true
+true
+  thats why.
