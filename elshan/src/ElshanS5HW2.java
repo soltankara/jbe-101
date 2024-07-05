359cac820029f156bed58dfb6c52c1f2d8abfff8
ElshanAlasgarov <aleskerof00@gmail.com>
2024-07-05 19:55:00 +0400

    Add files via upload

---
+++ elshan/src/ElshanS5HW2.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+// Eratosfen alqoritmi
+
+public class ElshanS5HW2{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = false; // 1 sadə ədəd deyil
+
+        int p = 2;
+        while (p * p <= n) {
+            if (isPrime[p - 1]) {
+                for (int i = p * p; i <= n; i += p) {
+                    isPrime[i - 1] = false;
+                }
+            }
+            p++;
+        }
+
+        System.out.println("Sadə ədədlər:");
+        for (int i = 0; i < n; i++) {
+            if (isPrime[i]) {
+                System.out.print((i + 1) + " ");
+            }
+        }
+    }
+}
