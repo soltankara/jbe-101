91b5dfc4d23dae63fecae0735e17e82058a5676e
Saleh Samadli <realmadrid.rm171@gmail.com>
2024-07-05 20:04:27 +0400

    Homework2 ready

--- saleh/src/Homework2.java
+++ saleh/src/Homework2.java
@@ -3,6 +3,30 @@ import java.util.Scanner;
 public class Homework2 {
     public static void main(String[] args) {
 
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter a text: ");
+//        String text = sc.nextLine();
+//
+//        System.out.print("Enter char you want to see index: ");
+//        String input = sc.nextLine();
+//
+//        char charToFind = input.charAt(0);
+//
+//        int index = -1;
+//        for (int i = 0; i < text.length(); i++) {
+//            if (text.charAt(i) == charToFind) {
+//                index = i;
+//                break;
+//            }
+//        }
+//
+//        if (index != -1) {
+//            System.out.printf("Index of '%c' in '%s' is: %d\n", charToFind, text, index);
+//        } else {
+//            System.out.printf("Character '%c' not found in '%s'\n", charToFind, text);
+//        }
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a text: ");
@@ -13,13 +37,7 @@ public class Homework2 {
 
         char charToFind = input.charAt(0);
 
-        int index = -1;
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == charToFind) {
-                index = i;
-                break;
-            }
-        }
+        int index = text.indexOf(charToFind);
 
         if (index != -1) {
             System.out.printf("Index of '%c' in '%s' is: %d\n", charToFind, text, index);
