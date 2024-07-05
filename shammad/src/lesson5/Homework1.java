5655055194ec2cebcb50038720bcf7efb1e825cd
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2024-07-05 15:48:14 +0400

    Shammad's homework

---
+++ shammad/src/lesson5/Homework1.java
@@ -0,0 +1,18 @@
+package lesson5;
+
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        //Note: program is case sensitive.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a text: ");
+        String text = scanner.nextLine();
+
+        StringBuilder sb = new StringBuilder(text);
+        sb.reverse();
+        System.out.println(sb);
+        System.out.println(text.equals(sb.toString()) ? (text + " is palindrome") : (text + " is not palindrome"));
+    }
+}
