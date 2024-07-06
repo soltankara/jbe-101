b9dc2f40b6412c11526efb7f7c32ae9832fbfa1f
Maharram <meherremheyderov04@gmail.com>
2024-07-06 15:36:57 +0400

    session5

---
+++ maharram/src/session5/Homew1.java
@@ -0,0 +1,24 @@
+package session5;
+
+import java.util.Scanner;
+
+public class Homew1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter text : ");
+        String text = sc.nextLine();
+
+        String reversetext = new StringBuilder(text).reverse().toString();
+
+        if (text.equalsIgnoreCase(reversetext)) {
+            System.out.println("text is palindrome");
+        } else {
+            System.out.println("text is not polindrome");
+        }
+        sc.close();
+
+
+    }
+
+}
