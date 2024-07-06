45de894bae3517221c5e90e953e84d4917e97eae
Maharram <meherremheyderov04@gmail.com>
2024-07-06 19:47:01 +0400

    meherremss5

--- maharram/src/session5/Homeww2.java
+++ maharram/src/session5/Homeww2.java
@@ -1,2 +1,45 @@
-package session5;public class Homeww2 {
+package session5;
+
+import java.util.Scanner;
+
+public class Homeww2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter number : ");
+        int number = sc.nextInt();
+
+        if (number <= 1) {
+            System.out.print("There is no prime number less than and equal to the number  ");
+        } else {
+            System.out.print("prime numbers less than or equal to : ");
+            for (int i = 0; i <= number; i++) {
+                if (primenumber(i)) {
+                    System.out.print(i + "  ");
+
+                }
+
+
+            }
+
+        }
+
+    }
+
+
+    public static boolean primenumber(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % 2 == 0) {
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+
+
 }
--- maharram/src/session5/Homeww3.java
+++ maharram/src/session5/Homeww3.java
@@ -1,2 +1,35 @@
-package session5;public class Homeww3 {
+package session5;
+
+import java.util.Scanner;
+
+public class Homeww3 {
+    public static void main(String[] args) {
+
+        //1.1. Write a Java program to get a number from the user and print whether it is positive or negative.
+        //
+        //Test Data
+        //Input number: 35
+        //Expected Output :
+        //Number is positive
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter number : ");
+        int number = sc.nextInt();
+
+        if (number >0){
+            System.out.print("number is positive");
+
+        }else if (number==0){
+            System.out.println("number is zero ");
+        }else if (number<0){
+            System.out.println("number is negative ");
+        }
+
+
+
+    }
 }
+
+
+
+
