2eb6e4a9b85b36cf5a1c538410c1a209bf656f60
Ayten <ababakishiyeva@std.beu.edu.az>
2024-07-06 18:27:50 +0300

    ayten-whileloops

---
+++ ayten/src/whileloophw1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class whileloophw1 {
+
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a text: ");
+            String s = sc.nextLine();
+            StringBuilder strb = new StringBuilder(s);
+            strb.reverse();
+            if (s.equals(String.valueOf(strb))) {
+                System.out.println(s + " is palindrome");
+            } else System.out.println(s + " is not palindrome");
+        }
+    }
+
