d05b627b87cdc665cd67bc5b72e17c1c178764e3
Adil İsmayılov <ismayilovadil670@gmail.com>
2024-07-07 21:19:08 +0400

    adil Ex9 new

--- adil/src/lesson06/HomeWorks/HW3/Ex9.java
+++ adil/src/lesson06/HomeWorks/HW3/Ex9.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class Ex9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean isLeap;
         System.out.print("Input the year: ");
         int year = scanner.nextInt();
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) System.out.println(year + " is a leap year");
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) System.out.println(year + " is a leap year");
         else System.out.println(year + " isn't a leap year");
     }
 }
