0475e4db4b68fbd09574c19ad4622035b7d0336d
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2024-07-07 18:48:34 +0400

    Shammad's CourseWorks Done.

--- shammad/src/session6coursework/CourseWork1.java
+++ shammad/src/session6coursework/CourseWork1.java
@@ -1,2 +1,23 @@
-package session6coursework;public class CourseWork1 {
+package session6coursework;
+
+import java.util.Scanner;
+
+public class CourseWork1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int sum = 0;
+        int temp = 0;
+
+        while (number != 0) {
+            temp = number % 10;
+            sum += temp;
+            number /= 10;
+        }
+
+        System.out.println("Sum of digits is: " + sum);
+    }
 }
--- shammad/src/session6coursework/CourseWork4.java
+++ shammad/src/session6coursework/CourseWork4.java
@@ -1,2 +1,38 @@
-package session6coursework;public class CourseWork4 {
+package session6coursework;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class CourseWork4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberToGuess = new Random().nextInt(0, 10);
+        int tries = 3;
+        int guess = 0;
+        System.out.println("You have 3 tries. Guess the number between 0 and 10");
+
+        while (tries != 0) {
+            System.out.println("Take a guess: ");
+            guess = scanner.nextInt();
+            if (guess == numberToGuess) {
+                System.out.println("Correct, you won! the number was ---> " + numberToGuess);
+                break;
+            } else {
+                tries--;
+                System.out.println("Wrong guess.");
+
+                if (tries == 0) {
+                    System.out.println("You failed to guess the number.");
+                    break;
+                }
+
+                if (guess > numberToGuess)
+                    System.out.println("Try lower number");
+                else
+                    System.out.println("Try higher number");
+            }
+        }
+        System.out.println("Number was: " + numberToGuess);
+    }
 }
