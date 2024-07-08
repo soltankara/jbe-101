2be531aa26c15766c94039a60387f391cf7c1a3c
JavadNazirli <javadnazirli00@gmail.com>
2024-07-08 12:51:41 +0400

    Update Book.java

--- javad/src/Task_day_3/Book.java
+++ javad/src/Task_day_3/Book.java
@@ -2,26 +2,27 @@ package Task_day_3;
 
 public class Book {
     public static void main(String[] args) {
-         String title ="Cinayet ve Ceza";      // Kitabın başlığı
-         String publishingHouse = "Qanun";
-         String topicType = "Dram";  // Janr
-         String author = "F. Dostoyevski";     // Yazar
-         int publishingYear = 2024;
-         int pageCount = 643;     // Sehife sayi
-         String isbn = "1902312912353"; // ISBN nomresi
-         long priceQpk = 1_100L;        // Qiymet (qepikle)
-         boolean available = true; // Movcut olup olmadığı;
-      //----------------------------------------------//
-        System.out.println("Book title :"+ title);
-        System.out.println("Publishing House :"+ publishingHouse);
-        System.out.println("Topic type (genre) :"+ topicType);
-        System.out.println("Author :"+ author);
-        System.out.println("Publishing year :"+publishingYear);
-        System.out.println("Page count :"+ pageCount);
-        System.out.println("ISBN no. :"+ isbn);
-        System.out.println("Price :"+ priceQEPIK);
-        System.out.println("Available  :"+ available);
+        String title = "Cinayet ve Ceza";      // Kitabın başlığı
+        String publishingHouse = "Qanun";
+        String topicType = "Dram";  // Janr
+        String author = "F. Dostoyevski";     // Yazar
+        int publishingYear = 2024;
+        int pageCount = 643;     // Sehife sayi
+        String isbn = "1902312912353"; // ISBN nomresi
+        long priceQpk = 1_100L;        // Qiymet (qepikle)
+        boolean available = true; // Movcut olup olmadığı;
+        //----------------------------------------------//
+        System.out.println("Book title :" + title);
+        System.out.println("Publishing House :" + publishingHouse);
+        System.out.println("Topic type (genre) :" + topicType);
+        System.out.println("Author :" + author);
+        System.out.println("Publishing year :" + publishingYear);
+        System.out.println("Page count :" + pageCount);
+        System.out.println("ISBN no. :" + isbn);
+        System.out.println("Price :" + priceQpk);
+        System.out.println("Available  :" + available);
 
 
     }
 }
+
