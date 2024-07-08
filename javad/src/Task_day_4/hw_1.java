8ed1286f3f96518daa86bcb3e82f64183ba14b47
JavadNazirli <javadnazirli00@gmail.com>
2024-07-08 12:58:43 +0400

    Update hw_1.java
    scanners optimized

--- javad/src/Task_day_4/hw_1.java
+++ javad/src/Task_day_4/hw_1.java
@@ -6,12 +6,10 @@ public class hw_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a text : ");
-        String text = sc.next();
+        String text = sc.nextLine();
         System.out.print("Enter a index to replace : ");
-        sc = new Scanner(System.in);
         int index = sc.nextInt();
         System.out.print("Enter a character you wanna replace with  : ");
-        sc = new Scanner(System.in);
         char ch = sc.next().charAt(0);
         char achar = text.charAt(index);
         String newstr = text.replace(achar,ch);
