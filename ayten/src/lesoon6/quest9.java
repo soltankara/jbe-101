9f1c5c0922d8a9966ede500ed07bf448de747081
Ayten <ababakishiyeva@std.beu.edu.az>
2024-07-09 17:58:44 +0300

    ayten-quest9

---
+++ ayten/src/lesoon6/quest9.java
@@ -0,0 +1,16 @@
+package src.lesoon6;
+
+import java.util.Scanner;
+
+public class quest9 {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        int year=scanner.nextInt();
+        boolean isleap;
+        if((year%4!=0 || year%400!=0)&&year%100==0) isleap=false;
+        else isleap=true;
+        if(isleap) System.out.println("leap");
+        else System.out.println("is'nt leap");
+    }
+}
