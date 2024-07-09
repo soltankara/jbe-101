ac724dd3afc8f22fd75e27d826616ab760551317
Saleh Samadli <realmadrid.rm171@gmail.com>
2024-07-09 19:03:14 +0400

    Homework session 4 ready

---
+++ saleh/src/session4/Homework2.java
@@ -0,0 +1,51 @@
+package session4;
+
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("Enter a text: ");
+//        String text = sc.nextLine();
+//
+//        System.out.print("Enter char you want to see index: ");
+//        String input = sc.nextLine();
+//
+//        char charToFind = input.charAt(0);
+//
+//        int index = -1;
+//        for (int i = 0; i < text.length(); i++) {
+//            if (text.charAt(i) == charToFind) {
+//                index = i;
+//                break;
+//            }
+//        }
+//
+//        if (index != -1) {
+//            System.out.printf("Index of '%c' in '%s' is: %d\n", charToFind, text, index);
+//        } else {
+//            System.out.printf("Character '%c' not found in '%s'\n", charToFind, text);
+//        }
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a text: ");
+        String text = sc.nextLine();
+
+        System.out.print("Enter char you want to see index: ");
+        String input = sc.nextLine();
+
+        char charToFind = input.charAt(0);
+
+        int index = text.indexOf(charToFind);
+
+        if (index != -1) {
+            System.out.printf("Index of '%c' in '%s' is: %d\n", charToFind, text, index);
+        } else {
+            System.out.printf("Character '%c' not found in '%s'\n", charToFind, text);
+        }
+
+    }
+}
---
+++ saleh/src/session4/Homework4.java
@@ -0,0 +1,47 @@
+package session4;
+
+import java.util.Scanner;
+
+public class Homework4 {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first integer: ");
+        num1 = sc.nextInt();
+
+        System.out.print("Enter the second integer: ");
+        num2 = sc.nextInt();
+
+        System.out.print("Choose an operation (+, -, *, /, %): ");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println(num1 + num2);
+                break;
+            case '-':
+                System.out.println(num1 - num2);
+                break;
+            case '*':
+                System.out.println(num1 * num2);
+                break;
+            case '/':
+                if (num2 != 0)
+                    System.out.println(num1 / num2);
+                else
+                    System.out.println("Error: Division by zero is not allowed.");
+                break;
+            case '%':
+                if (num2 != 0)
+                    System.out.println(num1 % num2);
+                else
+                    System.out.println("Error: Modulus by zero is not allowed.");
+                break;
+            default:
+                System.out.println("Invalid choice.");
+        }
+
+    }
+}
