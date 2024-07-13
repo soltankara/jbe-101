90782c5b7f726524491d36107218ade3d4c3672d
gurbanli <gurbanli009@gmail.com>
2024-07-13 14:15:45 +0400

    array codes

--- joshgun/src/session6/RemoveDup.java
+++ joshgun/src/session6/RemoveDup.java
@@ -4,17 +4,15 @@ package session6;
 // THere is a problem while printing the last number
 public class RemoveDup {
     public static void main(String[] args) {
-        int[] array = {1, 3, 6, 2, 3, 9, 19};
+        int[] array = {1, 3, 6, 2, 3, 6, 9, 19};
 
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] == array[j]) {
-                    for (int k = j; k < array.length - 1; k++) {
-                        array[k] = array[k + 1];
-                    }
+                    i++;
                 }
             }
+            System.out.print(array[i] + " ");
         }
-        for (int num : array) System.out.println(num);
     }
 }
