9e34be5013d9737f801a17db5ec3e6dae31ccab5
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2024-07-15 22:15:58 +0400

    homeworks and leetcode exercises done

---
+++ shammad/src/session8/Homework3.java
@@ -0,0 +1,24 @@
+package session8;
+
+public class Homework3 {
+    public static void main(String... args) {
+        System.out.println(reverse(-2312));
+    }
+
+    //LeetCode 7.Reverse Integer
+    static int reverse(int x) {
+        try {
+            StringBuilder sb = new StringBuilder(String.valueOf(x));
+            String prefix = "";
+            if (sb.charAt(0) == '-') {
+                prefix = "-";
+                sb.deleteCharAt(0);
+            }
+            sb.reverse();
+            return Integer.parseInt(prefix + sb);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+}
+
