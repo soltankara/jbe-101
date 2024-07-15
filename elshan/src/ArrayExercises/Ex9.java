1f0a2642c74b40738730cbb51a5f1769246641db
Elshan Alasgarov <aleskerof00@gmail.com>
2024-07-15 21:38:42 +0400

    Array Exercises

--- elshan/src/ArrayExercises/Ex9.java
+++ elshan/src/ArrayExercises/Ex9.java
@@ -5,15 +5,18 @@ import java.util.Arrays;
 public class Ex9 {
     public static void main(String[] args) {
         // 9. Write a Java program to insert an element (specific position) into an array.
-        int[] arr = {2, 3, 1, 5, 8};
+        int[] arr1 = {2, 3, 1, 5, 8};
+        int[] arr2 = new int[arr1.length + 1];
         int index = 1;
         int value = 9;
-
-        for (int i = arr.length -1 ; i < index ; i--) {
-            arr[i] = arr[i-1];
+        for (int i = 0,j = 0; i < arr1.length; i++,j++ ){
+            if(index == j){
+                arr2[j++] = value;
+            }
+            arr2[j] = arr1[i];
         }
-        arr [index] = value;
 
-        System.out.println(Arrays.toString(arr));
+
+        System.out.println(Arrays.toString(arr2));
     }
 }
