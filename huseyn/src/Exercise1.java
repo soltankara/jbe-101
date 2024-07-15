42fde2756d7e741ea575fd10801830875f509a8b
HuseynHeydarli <huseyn.heydarli2021@khazar.org>
2024-07-15 23:43:31 +0400

    Huseyn's first commit

--- huseyn/src/Exercise1.java
+++ huseyn/src/Exercise1.java
@@ -5,22 +5,22 @@ public class Exercise1 {
     public static void main(String[] args) {
         // 1. Write a Java program to sort a numeric array and a string array.
 
-        int[] num_array = {
+        int[] numArray = {
                 5, 7, 9, 2, 1
         };
 
-        String[] str_array = {
+        String[] strArray = {
                 "Besiktas",
                 "Galatasaray",
                 "Fenerbahce"
         };
 
 
-        Arrays.sort(num_array);
-        System.out.println(Arrays.toString(num_array));
+        Arrays.sort(numArray);
+        System.out.println(Arrays.toString(numArray));
 
 
-        Arrays.sort(str_array);
-        System.out.println(Arrays.toString(str_array));
+        Arrays.sort(strArray);
+        System.out.println(Arrays.toString(strArray));
     }
 }
