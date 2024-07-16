6d33c9c9091cfca3608cfded6d802c433af2dc4f
Ayten <ababakishiyeva@std.beu.edu.az>
2024-07-16 17:40:21 +0300

    methods added

---
+++ ayten/src/metods/IntToRom.java
@@ -0,0 +1,21 @@
+package metods;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class IntToRom {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int num= scanner.nextInt();
+       // System.out.println(integerToRoman(num));
+    }
+//    public static String integerToRoman(int num){
+//        String[] teklik={"","I","II","III","IV","V","VI","VII","VIII","IX"};
+//        String[] onluq={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XM"};
+//        String[] yuzluk={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+//        String [] minlik={"","M","MM","MMM"};
+//        return minlik[num/1000]+yuzluk[num%1000/100]+onluq[num%100/10]+teklik[num%10];
+//    }
+
+
+}
--- ayten/src/metods/iseven.java
+++ ayten/src/metods/iseven.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public class iseven {
     public static void main(String[] args) {
 
-        System.out.println(sum(5,2,6,8,9,3,3,7));
+        System.out.println(sum(5, 2, 6, 8, 9, 3, 3, 7));
     }
-//    static boolean isEven(int number){
+
+    //    static boolean isEven(int number){
 //        return number%2==0 ? true : false;
 //    }
 //    static int min(int num1,int num2,int num3){
@@ -27,10 +28,10 @@ public class iseven {
 //        }
 //return sum;
 //    }
-    public static int sum(int... arr){
-        if(arr.length==0) return 0;
-        int [] arrnew= Arrays.copyOfRange(arr,1,arr.length);
-        return  arr[0] +sum(arrnew);
+    public static int sum(int... arr) {
+        if (arr.length == 0) return 0;
+        int[] arrnew = Arrays.copyOfRange(arr, 1, arr.length);
+        return arr[0] + sum(arrnew);
     }
 
 
