2bd614e1ee039a3e2a535884ac7329df2a4d02e8
Maharram <meherremheyderov04@gmail.com>
2024-07-16 16:51:41 +0400

    session7methods-meherrem   the code has been corrected

--- maharram/src/Session7Methods/HomeTask2.java
+++ maharram/src/Session7Methods/HomeTask2.java
@@ -1,24 +1,15 @@
 package Session7Methods;
-import java.util.Scanner;
 public class HomeTask2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print(" how many numbers do you wnat to enter : ");
-        int sizeOfNumber = sc.nextInt();
-        int[] array = new int[sizeOfNumber];
-        System.out.println("enter numbers for sum ");
-        for (int i = 0; i < sizeOfNumber; i++) {
-            System.out.print("number " + (i + 1) + " : ");
-            array[i] = sc.nextInt();
-        }
-        int sumOfNumbers = sum(array, 0);
-        System.out.println("The sum of the numbers :" + sumOfNumbers);
+        System.out.println(sum(1, 2, 3, 4, 5));
     }
-    public static int sum(int[] arr, int num) {
-        if (num >= arr.length) {
+    public static int sum(int... arr) {
+        return sumWithRecursive(arr, arr.length - 1);
+    }
+    private static int sumWithRecursive(int[] numbers, int index) {
+        if (index < 0) {
             return 0;
-        } else {
-            return arr[num] + sum(arr, num + 1);
         }
+        return numbers[index] + sumWithRecursive(numbers, index - 1);
     }
 }
