796322b436461f86b8e820eb99e160a44afaabb1
Saleh Samadli <realmadrid.rm171@gmail.com>
2024-07-16 15:36:36 +0400

    Homework session 8 ready

---
+++ saleh/src/session8/HomeWork1.java
@@ -0,0 +1,40 @@
+package session8;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        int number = 5; // the number to calculate the sum up to
+
+        int sumUsingLoop = sumUsingLoop(number);
+
+        System.out.println("Sum of numbers up to " + number + " using loop is " + sumUsingLoop);
+
+        int sumUsingRecursion = sumUsingRecursion(number);
+
+        System.out.println("Sum of numbers up to " + number + " using recursion is " + sumUsingRecursion);
+    }
+
+    private static int sumUsingRecursion(int number) {
+        int sum = 0;
+        if (number == 1) {
+            return 1;
+        } else {
+            sum = sum + number;
+        }
+
+        return sum;
+    }
+
+    private static int sumUsingLoop(int number) {
+//        if (number == 1)
+//            return 1;
+//        else
+//            return number + sumUsingRecursion(number - 1);
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+        }
+        return sum;
+
+    }
+
+}
