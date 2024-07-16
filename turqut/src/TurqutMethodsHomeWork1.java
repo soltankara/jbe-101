ee79f34ed832226cbf7a0eeed7bc0bc67197134a
Turqut7 <turqutismayil007@gmail.com>
2024-07-16 15:57:36 +0400

    Methods Homeworks
    I solve for all homework with details.

---
+++ turqut/src/TurqutMethodsHomeWork1.java
@@ -0,0 +1,18 @@
+package turqut.src;
+
+import java.util.Arrays;
+
+public class TurqutMethodsHomeWork1 {
+    public static void main(String[] args) {
+        System.out.println(sumAllWithRecursion(3, 5, 7));
+
+    }
+
+    private static int sumAllWithRecursion(int... numbers) {
+        if (numbers.length == 0) {
+            return 0;
+        }
+        return numbers[0] + sumAllWithRecursion(Arrays.copyOfRange(numbers, 1, numbers.length));
+    }
+}
+
