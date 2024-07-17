17977362a723c66d99ec431f4c752fc6780c8c74
alievaleila <meliyeva452@gmail.com>
2024-07-17 22:23:18 +0400

    Leyla-Session-7

---
+++ leyla/src/Session8.java
@@ -0,0 +1,24 @@
+//MethodClasswork2
+package Session8;
+public class MethodsClasswork2 {
+    public static void main(String[] args) {
+        int number = 5; // the number to calculate the sum up to
+        int sumUsingLoop = sumUsingLoop(number);
+        System.out.println("Sum of numbers up to " + number + " using loop is " + sumUsingLoop);
+        int sumUsingRecursion = sumUsingRecursion(number);
+        System.out.println("Sum of numbers up to " + number + " using recursion is " + sumUsingRecursion);
+    }
+    private static int sumUsingRecursion(int number) {
+        if (number == 1) {
+            return 1;
+        }
+        return number + sumUsingRecursion(number - 1);
+    }
+    private static int sumUsingLoop(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
