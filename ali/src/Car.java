ba4fb0cc44b33e8d0e14bff05521c2cecc550dbf
Aliko <168551662+Aliko313@users.noreply.github.com>
2024-07-27 18:32:10 +0400

    Ali

---
+++ ali/src/Car.java
@@ -0,0 +1,31 @@
+public class Car {
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    private String model;
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    private String marka;
+
+    public int getIl() {
+        return il;
+    }
+
+    public void setIl(int il) {
+        this.il = il;
+    }
+
+    private int il;
+}
