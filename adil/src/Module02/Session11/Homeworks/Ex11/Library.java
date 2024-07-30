d75e66781e5665ba0e2850d0536c9008b028596b
Adil İsmayılov <ismayilovadil670@gmail.com>
2024-07-30 23:54:03 +0400

    adil-session11-Ex11

--- adil/src/Module02/Session11/Homeworks/Ex11/Library.java
+++ adil/src/Module02/Session11/Homeworks/Ex11/Library.java
@@ -1,24 +1,30 @@
 package Module02.Session11.Homeworks.Ex11;
 
+import java.util.Arrays;
+
 public class Library {
-    private final Book[] books = new Book[1000];
+    private Book[] books = new Book[2];
     private static int k = 0;
 
     public void add(Book... bookS) {
         for (Book book : bookS) {
+            if (k == books.length) {
+                books = Arrays.copyOf(books, books.length * 2);
+            }
             books[k++] = book;
         }
     }
 
-    public void remove(int id) {
-        for (int i = 0; i < books.length; i++) {
-            if (books[i].getId() == id) {
-                books[i] = null;
-                break;
+    public void remove(int... ids) {
+        for (int id : ids) {
+            for (int i = 0; i < books.length; i++) {
+                if (books[i] != null && books[i].getId() == id) {
+                    books[i] = null;
+                    break;
+                }
             }
         }
     }
-
     public void showLibrary() {
         for (Book book : books) {
             if (book != null) {
@@ -26,5 +32,4 @@ public class Library {
             }
         }
     }
-
 }
--- adil/src/Module02/Session11/Homeworks/Ex11/Main.java
+++ adil/src/Module02/Session11/Homeworks/Ex11/Main.java
@@ -6,8 +6,10 @@ public class Main {
         Book book1 = new Book("Marvel", "Stan Lee", 2000);
         Book book2 = new Book("Sherlock Holmes", "Sir Arthur", 2010);
         Book book3 = new Book("Sherlock Holmes", "Sir Arthur", 2011);
-        library.add(book1, book2, book3);
-        library.remove(1);
+        Book book4 = new Book("Sherlock Holmes", "Sir Arthur", 2012);
+        Book book5 = new Book("Sherlock Holmes", "Sir Arthur", 2013);
+        library.add(book1, book2, book3, book4, book5);
+        library.remove(1, 3);
         library.showLibrary();
     }
 }
