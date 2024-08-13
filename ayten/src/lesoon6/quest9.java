b1034f6f918c65c1a720555df11ac83dc7487f7a
gurbanli <gurbanli009@gmail.com>
2024-08-13 17:20:28 +0400

    session12

--- ayten/src/lesoon6/quest9.java
+++ ayten/src/lesoon6/quest9.java
@@ -2,15 +2,23 @@ package lesoon6;
 
 import java.util.Scanner;
 
-public class quest9 {
+public class Quest9 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the year: ");
+        int year = in.nextInt();
 
-        Scanner scanner = new Scanner(System.in);
-        int year = scanner.nextInt();
-        boolean isleap;
-        if ((year % 4 != 0 || year % 400 != 0) && year % 100 == 0) isleap = false;
-        else isleap = true;
-        if (isleap) System.out.println("leap");
-        else System.out.println("is'nt leap");
+        boolean x = (year % 4) == 0;
+        boolean y = (year % 100) != 0;
+        boolean z = ((year % 100 == 0) && (year % 400 == 0));
+
+        if (x && (y || z))
+        {
+            System.out.println(year + " is a leap year");
+        }
+        else
+        {
+            System.out.println(year + " is not a leap year");
+        }
     }
 }
