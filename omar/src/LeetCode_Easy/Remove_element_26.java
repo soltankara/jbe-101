b7d17849723f6bc78747e94a17206343795f7b92
OmarTayyar <147808423+OmarTayyar@users.noreply.github.com>
2024-08-20 16:26:10 +0400

    changed

--- omar/src/LeetCode_Easy/Remove_element_26.java
+++ omar/src/LeetCode_Easy/Remove_element_26.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 public class Remove_element_26 {
     public static void main(String[] args) {
         int[] arr = {5, 5, 6, 6, 3, 3, 4, 4, 2, 4, 5, 6, 9};
-        removeElement(arr, 6);
+        System.out.println(removeElement(arr, 6));
     }
 
     public static int removeElement(int[] nums, int val) {
-        int target = val;
-        Arrays.sort(nums);
+
         int[] arr = new int[nums.length];
-        for (int i = 0, k = 0; i < arr.length; i++) {
-            if (arr[i] != target) {
+        int k =0;
+        for (int i = 0; i < arr.length; i++) {
+            if (nums[i] != val) {
                 arr[k] = nums[i];
                 k++;
             }
@@ -21,7 +21,7 @@ public class Remove_element_26 {
         // mentiqe esasen for icindeki kod target elementi remove edir sualda sorusur ki yeni yaratdigim arrayin
         // element sayini cap et yeni uzunluguunu amma cap olunmur :(((
 
-        return arr.length;
+        return arr.length -(arr.length - k);
     }
 }
 
