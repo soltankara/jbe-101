eb5b4bd5d94cffff37ab58c4f134d6a14ffbd123
Ayten <ababakishiyeva@std.beu.edu.az>
2024-08-26 16:39:45 +0300

    resolved comments

--- ayten/src/metods/SumUsingRecursion.java
+++ ayten/src/metods/SumUsingRecursion.java
@@ -1,8 +1,6 @@
 package metods;
 
-import java.util.Arrays;
-
-public class Sumusingrecur {
+public class SumUsingRecursion {
     public static void main(String[] args) {
         int [][] arr={{2,3,4},{5,6,8},{9,7,1}};
         changingNiDegre(arr);
