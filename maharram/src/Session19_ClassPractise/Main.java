425ac989d3bc880bf6d8d3ed18113f67d4d99a10
Maharram <meherremheyderov04@gmail.com>
2024-08-27 16:40:56 +0400

    session19-2_Practise_Meherrem

--- maharram/src/Session19_ClassPractise/Main.java
+++ maharram/src/Session19_ClassPractise/Main.java
@@ -1,41 +1,36 @@
 package Session19_ClassPractise;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        List<Student> students = new ArrayList<>();
+        Student student = new Student();
+        student.students.add(new Student("Saleh", Grade.E));
+        student.students.add(new Student("Araz", Grade.C));
+        student.students.add(new Student("Sabuhi", Grade.B));
+        student.students.add(new Student("Javad", Grade.D));
+        student.students.add(new Student("Kanan", Grade.B));
+        student.students.add(new Student("Maharram", Grade.E));
 
-        students.add(new Student("Saleh", Grade.E));
-        students.add(new Student("Araz", Grade.C));
-        students.add(new Student("Sabuhi", Grade.B));
-        students.add(new Student("Javad", Grade.D));
-        students.add(new Student("Kanan", Grade.B));
-        students.add(new Student("Maharram", Grade.E));
-
-        System.out.println("find student by name (Maharram) :" + Student.findByName(students, "Maharram"));
-        System.out.println("find student by grade (B) :" + Student.findByGrade(students, Grade.B));
-        Map<Grade, List<Student>> gradeMap = Student.groupStudentByGrade(students);
+        System.out.println("find student by name (Maharram) :" + StudentOperations.findByName(student.students, "Maharram"));
+        System.out.println("find student by grade (B) :" + StudentOperations.findByGrade(student.students, Grade.B));
+        Map<Grade, List<Student>> gradeMap = StudentOperations.groupStudentByGrade(student.students);
         System.out.println("group students by grade " + gradeMap);
-        Student.removeByNameContains(students, "a");
-        System.out.println("remove student by text contains(a) :" + students);
+        StudentOperations.removeByNameContains(student.students, "a");
+        System.out.println("remove student by text contains(a) :" + student.students);
         System.out.println("--------------------------------------------------------------------------------------");
 
-        List<Student> studentsForStream = new ArrayList<>();
-        studentsForStream.add(new Student("Muhammed", Grade.E));
-        studentsForStream.add(new Student("Ali", Grade.C));
-        studentsForStream.add(new Student("Turqut", Grade.B));
-        studentsForStream.add(new Student("Omar", Grade.D));
-        studentsForStream.add(new Student("Bahruz", Grade.B));
-        studentsForStream.add(new Student("Maharram", Grade.E));
+        student.students.add(new Student("Muhammed", Grade.E));
+        student.students.add(new Student("Ali", Grade.C));
+        student.students.add(new Student("Turqut", Grade.B));
+        student.students.add(new Student("Omar", Grade.D));
+        student.students.add(new Student("Bahruz", Grade.B));
+        student.students.add(new Student("Maharram", Grade.E));
 
-        System.out.println("find student by name (Maharram) :" + StudentWithStream.findByName(studentsForStream, "Maharram"));
-        System.out.println("find student by grade (B) :" + StudentWithStream.findByGrade(studentsForStream, Grade.B));
-        Map<Grade, List<Student>> gradeMap1 = StudentWithStream.groupStudentByGrade(studentsForStream);
+        System.out.println("find student by name (Maharram) :" + StudentOperations.findByNameWithStream(student.students, "Maharram"));
+        System.out.println("find student by grade (B) :" + StudentOperations.findByGradeWithStream(student.students, Grade.B));
+        Map<Grade, List<Student>> gradeMap1 = StudentOperations.groupStudentByGradeWithStream(student.students);
         System.out.println("group students by grade " + gradeMap1);
-        StudentWithStream.removeByNameContains(studentsForStream, "a");
-        System.out.println("remove student by text contains(a) :" + studentsForStream);
+        StudentOperations.removeByNameContainsWithStream(student.students, "a");
+        System.out.println("remove student by text contains(a) :" + student.students);
     }
 }
--- maharram/src/Session19_ClassPractise/Student.java
+++ maharram/src/Session19_ClassPractise/Student.java
@@ -1,13 +1,14 @@
 package Session19_ClassPractise;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Student {
+    List<Student> students = new ArrayList<>();
     private String name;
     private Grade grade;
+
+    public Student() {
+    }
 
     public Student(String name, Grade grade) {
         this.name = name;
@@ -28,36 +29,5 @@ public class Student {
                 "name='" + name + '\'' +
                 ", grade=" + grade +
                 '}';
-    }
-
-    public static Student findByName(List<Student> students, String name) {
-        for (Student student : students) {
-            if (student.getName().equalsIgnoreCase(name)) {
-                return student;
-            }
-        }
-        return null;
-    }
-
-    public static List<Student> findByGrade(List<Student> students, Grade grade) {
-        List<Student> result = new ArrayList<>();
-        for (Student student : students) {
-            if (student.getGrade() == grade) {
-                result.add(student);
-            }
-        }
-        return result;
-    }
-
-    public static void removeByNameContains(List<Student> students, String text) {
-        students.removeIf(student -> student.getName().contains(text));
-    }
-
-    public static Map<Grade, List<Student>> groupStudentByGrade(List<Student> students) {
-        Map<Grade, List<Student>> gradeMap = new HashMap<>();
-        for (Student student : students) {
-            gradeMap.computeIfAbsent(student.getGrade(), k -> new ArrayList<>()).add(student);
-        }
-        return gradeMap;
     }
 }
