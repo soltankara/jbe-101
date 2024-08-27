62b3c3daf7a757ae0e7a0e43c7c946354a4c0be3
Ayten <ababakishiyeva@std.beu.edu.az>
2024-08-27 13:34:25 +0300

    fixed the method

--- ayten/src/ClassWork/Main.java
+++ ayten/src/ClassWork/Main.java
@@ -9,8 +9,12 @@ public class Main {
     public static void main(String[] args) {
         Student Leyla = new Student("Leyla", Grade.A);
         Student Nurcan = new Student("Nurcan", Grade.B);
+        Student Aysel=new Student("Aysel",Grade.C);
+        Student Nurana=new Student("Nurana",Grade.C);
         students.add(Leyla);
         students.add(Nurcan);
+        students.add(Nurana);
+        students.add(Aysel);
         Map<Grade, List<Student>> studentMap = new HashMap<>();
         for (Student student : students) {
 //            List<Student> mapList=studentMap.get(student.getGrade());
@@ -22,20 +26,22 @@ public class Main {
             Map<Grade, List<Student>> map = students.stream().collect(Collectors.groupingBy(Student::getGrade));
 
         }
-        removeByName("Leyla", students);
+
+        removeByName("Leyla");
         System.out.println("after removing");
         students.forEach(student -> System.out.println(student.getGrade() + " " + student.getName()));
-
+        System.out.println(findByName("Aysel"));
+        System.out.println(findingByGrade(Grade.C));
     }
 
-    static Optional<Student> findByName(String name, List<Student> students) {
+    static Optional<Student> findByName(String name) {
         for (Student student : students) {
             if (student.getName().equalsIgnoreCase(name)) return Optional.of(student);
         }
         return Optional.empty();
     }
 
-    static List<Student> findingByGrade(Grade grade, List<Student> students) {
+    static List<Student> findingByGrade(Grade grade) {
         List<Student> update = new ArrayList<>();
         for (Student student : students) {
             if (student.getGrade() == grade) update.add(student);
@@ -43,7 +49,7 @@ public class Main {
         return update;
     }
 
-    public static void removeByName(String name, List<Student> students) {
+    public static void removeByName(String name) {
         students.removeIf(student -> student.getName().equalsIgnoreCase(name));
     }
 
