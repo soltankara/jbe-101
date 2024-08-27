f235d513948198971c313b26d3a59fbf2cd56333
kenanabbaszade <kenanabbasszade@gmail.com>
2024-08-27 16:55:47 +0400

    5 Task in Pdf

---
+++ kanan/src/Session20/UpperCaseConverter.java
@@ -0,0 +1,21 @@
+package Session20;
+
+import java.util.Arrays;
+
+import java.util.List;
+
+import java.util.stream.Collectors;
+
+
+public class UpperCaseConverter {
+    public static void main(String[] args) {
+
+        List<String> inputWords = Arrays.asList("java","Kenan", "stream", "exercise");
+
+        List<String> upperCaseWords = inputWords.stream()
+                .map(String::toUpperCase)
+                .toList();
+
+        System.out.println("UpperCasedWords : " + upperCaseWords);
+    }
+}
