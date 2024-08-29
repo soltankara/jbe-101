809fae3a010d5b96a21454a25adabe2c06262994
kenanabbaszade <kenanabbasszade@gmail.com>
2024-08-29 13:25:16 +0400

    Ferq izahı updated

--- kanan/src/Session20/FirstElement.java
+++ kanan/src/Session20/FirstElement.java
@@ -8,11 +8,8 @@ public class FirstElement {
     public static void main(String[] args) {
         List<Integer> elements = Arrays.asList(1, 2, 6, 7, 3, 8);
         Optional<Integer> greaterElement = elements.stream()
-
                 .filter(n -> n > 5)
                 .findFirst();
-
-
         System.out.println("first element greater than five is : " + greaterElement);
     }
 }
