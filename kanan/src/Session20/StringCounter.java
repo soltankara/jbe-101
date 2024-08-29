c1978f0f63c71feb55102de4e812c246ce47b0ea
kenanabbaszade <kenanabbasszade@gmail.com>
2024-08-29 13:24:29 +0400

    Ferq izahı updated

--- kanan/src/Session20/StringCounter.java
+++ kanan/src/Session20/StringCounter.java
@@ -4,13 +4,10 @@ import java.util.List;
 
 public class StringCounter {
     public static void main(String[] args) {
-
         List<String> givenWords = Arrays.asList("Java", "sTream", "api", "code", "Kenan");
-
         long count = givenWords.stream()
                 .filter(perWord -> perWord.length() > 3)
                 .count();
-
         System.out.println("String Count is : " + count);
     }
 }
