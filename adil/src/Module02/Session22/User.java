b61e15cb68f615245bd3e7367770b7874a5fd623
Adil İsmayılov <ismayilovadil670@gmail.com>
2024-08-31 15:39:41 +0400

    adil session22 classwork

---
+++ adil/src/Module02/Session22/User.java
@@ -0,0 +1,78 @@
+package Module02.Session22;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class User implements Serializable {
+    private String name;
+    private String surname;
+    private String username;
+    private transient String password;
+    private Instant createdAt;
+
+    private User() {
+    }
+
+    private User(String name, String surname, String username, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+        this.createdAt = Instant.now();
+    }
+
+    public static User of(String name, String surname, String username, String password) {
+        return new User(name, surname, username, password);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
